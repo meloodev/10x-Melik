f82c7e273f0e2dd2f297f11f4932f897f8de7fc8
Melik <melikpapikyan@gmail.com>
2025-04-02 11:46:17 +0400

    passwords challenge

--- session24/hack-nasa/js/app.js
+++ session24/hack-nasa/js/app.js
@@ -17,8 +17,24 @@ document.addEventListener('DOMContentLoaded', () => {
         return st;
     }
 
+    function funcDecr(encryptedText, key) {
+        let encryptedCodes = encryptedText.split('-').map(Number);
+         let keyCodes = Array.from(key).map(char => char.charCodeAt(0));
+        let decrypted = encryptedCodes.map((code, index) => {
+            let keyCode = keyCodes[index % keyCodes.length];
+            return String.fromCharCode(code - keyCode);
+        });
+        
+        return decrypted.join('');
+    }
+
+
     let login = '';
     let password = '';
+
+    const idOne = '166-161-167-201-147-211-152-154-164-228';
+    const idTwo = '98-102-138-200-100-194-110-112-112-214-216-154-98-104-106';
+
 
     const btn = document.querySelector('.form button');
     btn.addEventListener('click', (e) => {
@@ -26,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
         login = document.querySelector('.form .form__login input').value;
         password = document.querySelector('.form .form__creditial input').value;
 
-        if (login !== null && login.length > 0 && login === myFunc('tmlsaiuarh5') && password !== null && password.length > 0 && password === myFunc('zohoyovoospl6')) {
+        if (login !== null && login.length > 0 && login === funcDecr(idOne, password) && password !== null && password.length > 0 && password === funcDecr(idTwo, password)) {
             const span = document.createElement('span');
             span.textContent = myFunc('Acs rne!cesgatd8');
             document.querySelector('form .result').textContent = '';
