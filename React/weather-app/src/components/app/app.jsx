d364512fb4b7b14b0eaccbf05a40804d56bb5130
Melik <melikpapikyan@gmail.com>
2025-07-22 00:15:13 +0400

    replace weather-app project to react

---
+++ React/weather-app/src/components/app/app.jsx
@@ -0,0 +1,9 @@
+import AppHeader from "../appHeader/appHeader";
+
+const App = () => {
+    return (
+        <AppHeader />
+    )
+}
+
+export default App;
---
+++ React/weather-app/src/components/appHeader/AppHeader.jsx
@@ -0,0 +1,50 @@
+import './AppHeader.scss';
+
+const AppHeader = () => {
+    return (
+        <header className="weather">
+            <div className="container">
+                <div className="weather__header">
+                    <div className="weather__header-top">
+                        <h1>Weather App</h1>
+                    </div>
+
+                    <div className="weather__header__icons">
+                        <div className="header__icon-cover menu">
+
+                            <i role="button" aria-label="Open settings menu" className="fa-solid fa-ellipsis-vertical"></i>
+
+
+                            <ul className="settings">
+                                <li className="weather__settings">
+                                    <button>settings</button>
+                                </li>
+                                <li className="weather__theme">
+                                    <i className="fa-solid fa-chevron-left"></i>
+                                    <button aria-haspopup="true">themes</button>
+                                    <ul className="submenu">
+                                        <li className="dark">
+                                            <button>dark</button>
+                                            <i className="fa-solid"></i>
+                                        </li>
+                                        <li className="light">
+                                            <button>light</button>
+                                            <i className="fa-solid fa-check"></i>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="loader__cover"></div>
+            <div className="loader"></div>
+            <div className="err__message">
+                <span>something went wrong</span>
+            </div>
+        </header>
+    )
+}
+
+export default AppHeader;
